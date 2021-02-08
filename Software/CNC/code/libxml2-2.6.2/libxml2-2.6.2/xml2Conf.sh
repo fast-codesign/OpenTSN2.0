b6188c99061a7a9361f74049e6799469dc067694
sunyinhan <hnu_syh@163.com>
2021-02-08 22:15:29 +0800

    update sofetware source code

---
+++ Software/CNC/code/libxml2-2.6.2/libxml2-2.6.2/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L${libdir}"
+XML2_LIBS="-lxml2 -lz -lpthread  -lm "
+XML2_INCLUDEDIR="-I${includedir}/libxml2"
+MODULE_VERSION="xml2-2.6.2"
+
